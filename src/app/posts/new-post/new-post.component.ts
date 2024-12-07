6d1d14c582bd8ac4d4e637ec11ff47bdb2ff5f90
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2024-12-07 14:40:24 +0530

    post image section

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -5,18 +6,29 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
 
   permalink: string = ''
+  imgSrc: any = '/placeholder-image.png'
+  selectedImg: any
 
   onTitleChanged(event: any) {
     const title = event.target.value
     this.permalink = title.replace(/\s/g, '-')
 
+  }
+
+  showPreview($event: any){
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result
+    }
+    reader.readAsDataURL($event.target.files[0])
+    this.selectedImg = $event.target.files[0]
   }
 
 }
